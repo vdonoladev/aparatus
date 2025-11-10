2d3e694d157b0cd00ccb2b2ac35dca1f6e3a4cc6
vdonoladev <victordonolaf@gmail.com>
2025-11-10 11:43:10 -0300

    feat: add booking item component

---
+++ app/_components/booking-item.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import { Badge } from "./ui/badge";
+import { Card } from "./ui/card";
+import { Avatar, AvatarImage } from "./ui/avatar";
+
+interface BookingItemProps {
+  serviceName: string;
+  barbershopName: string;
+  barbershopImageUrl: string;
+  date: Date;
+}
+
+const BookingItem = ({
+  serviceName,
+  barbershopName,
+  barbershopImageUrl,
+  date,
+}) => {
+  return (
+    <Card className="flex h-full w-full min-w-full flex-row items-center justify-between p-0">
+      {/* ESQUERDA */}
+      <div className="flex flex-1 flex-col gap-4 p-4">
+        <Badge>Confirmado</Badge>
+
+        <div className="flex flex-col gap-2">
+          <p className="font-bold">{serviceName}</p>
+          <div className="flex items-center gap-2">
+            <Avatar className="h-6 w-6">
+              <AvatarImage src={barbershopImageUrl} />
+            </Avatar>
+            <p className="text-muted-foreground text-sm">{barbershopName}</p>
+          </div>
+        </div>
+      </div>
+
+      {/* DIREITA */}
+      <div className="flex h-full flex-col items-center justify-center border-l p-4 py-3">
+        <p className="text-xs capitalize">
+          {date.toLocaleDateString("pt-BR", { month: "long" })}
+        </p>
+        <p>{date.toLocaleDateString("pt-BR", { day: "2-digit" })}</p>
+        <p className="text-xs capitalize">
+          {date.toLocaleTimeString("pt-BR", {
+            hour: "2-digit",
+            minute: "2-digit",
+          })}
+        </p>
+      </div>
+    </Card>
+  );
+};
+
+export default BookingItem;
--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,11 @@ import Header from "./_components/header";
 import SearchInput from "./_components/search-inputs";
 
 import banner from "../public/banner.png";
+import BookingItem from "./_components/booking-item";
 
 const Home = () => {
   return (
-    <div>
+    <main>
       <Header />
       <div className="space-y-4 px-5">
         <SearchInput />
@@ -16,8 +17,17 @@ const Home = () => {
           sizes="100vw"
           className="h-auto w-full"
         />
+        <h2 className="text-foreground text-xs font-semibold uppercase">
+          Agendamentos
+        </h2>
+        <BookingItem
+          serviceName="Corte de cabelo"
+          barbershopName="Barbearia do João"
+          barbershopImageUrl="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
+          date={new Date()}
+        />
       </div>
-    </div>
+    </main>
   );
 };
 
