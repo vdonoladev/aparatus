56fde30bda400433dc6a63f9d76ed523b36c6cd7
vdonoladev <victordonolaf@gmail.com>
2025-11-10 11:00:13 -0300

    feat: add header and image components

---
+++ app/_components/search-inputs.tsx
@@ -0,0 +1,20 @@
+import { SearchIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
+const SearchInput = () => {
+  return (
+    <div className="flex items-center gap-2">
+      <Input
+        type="text"
+        placeholder="Pesquise serviços ou barbearias"
+        className="border-border rounded-full"
+      />
+      <Button variant="default" size="icon" className="rounded-full">
+        <SearchIcon />
+      </Button>
+    </div>
+  );
+};
+
+export default SearchInput;
